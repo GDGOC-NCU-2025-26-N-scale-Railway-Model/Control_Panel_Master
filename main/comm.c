087703247b93b40547bb1935da7130667e87986f
Shawn Lin <spig100.roc@gmail.com>
2026-01-08 17:19:14 +0800

    feat: Finish the initialization functions partially

--- main/comm.c
+++ main/comm.c
@@ -2,26 +2,70 @@
 #include "custom_protocol.h"
 #include "esp_err.h"
 #include "esp_now.h"
+#include "esp_wifi_types_generic.h"
+#include "esp_netif.h"
+#include "esp_event.h"
+#include "esp_wifi.h"
 #include <string.h>
+#include "freertos/semphr.h"
+#include "freertos/timers.h"
+#include "esp_log.h"
+
+const char *TAG = "comm:";
+
+void wifi_init(void){
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(ESPNOW_WIFI_MODE));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_set_channel(0x0, WIFI_SECOND_CHAN_NONE));
+}
 
 void comm_init(void){
-    esp_err_t status = esp_now_init();
-    if (status != ESP_OK){
-        char* err = malloc(sizeof(esp_err_to_name(status)));
-        strcpy_s(
-            err, 
-            sizeof(esp_err_to_name(status)), 
-            esp_err_to_name(status));
-        printf("The ESP-NOW is not initialized succefully. Error: %s", err);
-        return;
-    }
-    printf("The ESP-NOW has been initialized successfully.");
+
+    ESP_ERROR_CHECK(esp_now_init());
+    printf("The ESP-NOW has been initialized successfully.");    
     return;
+}
+
+void on_data_sent(const uint8_t *mac_addr, esp_now_send_status_t status){
+        ESP_LOGI(TAG, "Last packet send status: %s", status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+
+void comm_peer_setup(void){
+    peer_t peers[] = PEER_LIST;
+    
+    for(int i = 0; i < PEER_NUM; i++){
+        esp_now_peer_info_t peer_handler;
+        memcpy(peer_handler.peer_addr, peers[i].mac, sizeof(peer_handler.peer_addr)); 
+        /*Note: List can not be directly assigned, memcpy is required*/
+        peer_handler.channel = peers[i].channel;
+        peer_handler.ifidx = WIFI_IF_STA;
+        peer_handler.encrypt = false;
+        
+        ESP_ERROR_CHECK(esp_now_add_peer(&peer_handler));
+    }
 }
 
 /*TODO: Finish the test sending and de-initialization*/
 
-esp_err_t send_test(const uint8_t *peer_mac, test_data_t *data){
-    esp_err_t hold = 0x000;
-    return hold;
+void send_task(void *pvParameter){
+
+}
+
+esp_err_t send_test(const uint8_t *peer_addr, test_data_t *data){
+    
+}
+
+void comm_deinit(peer_t *peers){
+    for(uint8_t i = 0; i < PEER_NUM; i++){
+        ESP_ERROR_CHECK(esp_now_del_peer(peers->mac));
+    }
+
+    ESP_ERROR_CHECK(esp_now_deinit());
+
 }
--- main/comm.h
+++ main/comm.h
@@ -1,12 +1,38 @@
 #ifndef COMM_H
 #define COMM_H
 
+/*Here we used preprocessor to add a initializer list.*/
+
+#define PEER_LIST { \
+    {{0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 1} \
+}
+
+#define PEER_NUM 1
+
+#define ESPNOW_WIFI_MODE    WIFI_MODE_STA
+#define ESPNOW_WIFI_IF      ESP_IF_WIFI_STA
+
+
 #include "esp_err.h"
 #include "custom_protocol.h"
+#include "esp_now.h"
 #include <stdint.h>
 
+typedef struct{
+    uint8_t mac[6];
+    int channel;
+} peer_t;
+
+typedef struct {
+
+} on_data_sent_cb;
+
+void wifi_init(void);
 void comm_init(void);
-esp_err_t send_test(const uint8_t *peer_mac, test_data_t *data);
-void comm_deinit(void);
+void send_task(void *pvParameter);
+void comm_peer_setup(void);
+esp_err_t send_test(const uint8_t *peer_addr, test_data_t *data);
+void comm_deinit(peer_t *peers);
+void on_data_sent(const uint8_t *mac_addr, esp_now_send_status_t status);
 
 #endif
